a012290a58f42a351b22bc509a6e4fad1ff1cc94
xuanhieu12345 <hieuxuan213@gmail.com>
2026-06-23 17:36:53 +0700

    search

--- src/test/java/dataproviders/SearchDataProvider.java
+++ src/test/java/dataproviders/SearchDataProvider.java
@@ -24,8 +24,12 @@ public class SearchDataProvider {
                 // "Tìm kiếm với ký tự đặc biệt") },
                 { new SearchData("sadasdasd123123", "Ôi! Rất tiếc không tìm thấy sản phẩm nào...!",
                         "Tìm kiếm với từ khóa không tồn tại") },
-                { new SearchData("   ", "Vui lòng nhập từ khóa", "Tìm kiếm chỉ chứa khoảng trắng") },
+                { new SearchData("NVIDIATCKAIECSDFGHGHJSE GeForce RTXTHCTCEXSE 40800283",
+                        "Ôi! Rất tiếc không tìm thấy sản phẩm nào...!",
+                        "Tìm kiếm sản phẩm cụ thể") },
                 { new SearchData("<script>alert(1)</script>", "Ôi! Rất tiếc", "Kiểm thử bảo mật XSS Injection") },
+                { new SearchData("123123123123123123123123123",
+                        "Ôi! Rất tiếc không tìm thấy sản phẩm nào...!", "Tìm kiếm với chuỗi ký tự số dài") },
                 // { new SearchData("' OR 1=1 --", "Ôi! Rất tiếc", "Kiểm thử bảo mật SQL
                 // Injection") },
         };
